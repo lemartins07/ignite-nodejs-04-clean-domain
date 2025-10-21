8bc60f35a988fbb43a80df8309732da7391fa49e
Leandro Martins <leandro.martins.85@gmail.com>
2025-10-21 20:51:02 -0300

    Fundamentos de DDD - Abstraindo criação de entidades

---
+++ src/domain/use-cases/answer-question.spec.ts
@@ -0,0 +1,21 @@
+import { expect, test } from 'vitest'
+
+import { Answer } from '../entities/answer'
+import { AnswersRepository } from '../repositories/answers-repository'
+import { AnswerQuestionUseCase } from './answer-questions'
+
+const fakeAnswersRepository: AnswersRepository = {
+  create: async (answer: Answer) => {},
+}
+
+test('create an answer', async () => {
+  const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository)
+
+  const answer = await answerQuestion.execute({
+    questionId: '1',
+    instructorId: '1',
+    content: 'Nova resposta',
+  })
+
+  expect(answer.content).toEqual('Nova resposta')
+})
